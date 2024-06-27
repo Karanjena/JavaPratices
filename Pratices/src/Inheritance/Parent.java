31bbd74c4a2c823be75033d30634d7e49b669d06
Karan <Karan@DESKTOP-L5TSS0V>
2024-06-27 17:19:22 +0530

    praties

---
+++ Pratices/src/Inheritance/Parent.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Parent {
+
+	int a=10;
+	static int  b=15;
+	public static void temp() {
+		System.out.println("Parent class static method");
+	}
+	public void demo() {
+		System.out.println("parent class non static method");
+	}
+}
---
+++ Pratices/src/Inheritance/child.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class child extends Parent{
+public static void main(String[] args) {
+	child c=new child();
+	System.out.println(c.a);
+	System.out.println(c.b);
+	c.temp();
+	c.demo();
+}
+}
+
+
